ad75f4f1658ebbf14c5ddb4e4bd09d76fad8a017
beniamino nobile <beniaminonobile@ioinvesto.net>
2025-06-04 12:24:16 +0200

    updated git workflows

--- tests/dummy_settings.py
+++ tests/dummy_settings.py
@@ -1,11 +1,14 @@
+import os
+import dj_database_url
+
 SECRET_KEY = "dummy"
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django_lastdayofmonth",
 ]
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///:memory:"
+    )
 }
